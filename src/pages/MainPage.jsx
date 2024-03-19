a8eba25d76d56c8c806395ea3ce36fc9ec2ee64b
subbi <92204014+subinsad@users.noreply.github.com>
2024-03-19 11:27:55 +0900

    chore : main으로 변경

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,10 @@
+import Badge from '../components/Common/Badge';
+
+export default function Main() {
+    return (
+        <>
+            <Badge text="-50%" className="text-rose-600" />
+            <Badge text="New" className="bg-blue-500 text-white " />
+        </>
+    );
+}
