ddfc679272d5b1e6a9505e8ecdaaabfd49556da8
김민재 <kimminjae@gimminjaeui-MacBookPro.local>
2024-03-21 02:03:41 +0900

    feat:로그인/회원가입 기능구현 중간커밋

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,21 +1,74 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../components/Common/Button.jsx";
 import TItle from "../components/Common/Title";
 import Input from "../components/Form/Input";
+import { useState } from "react";
 
 export default function SignInPage() {
+  /// 로그인폼에 상태를 객체로 관리
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: "",
+    error: "",
+  });
+  const navigate = useNavigate;
+
+  /// 로그인함수
+  const login = async (email, password) => {
+    const result = await axios.post("", { email, password });
+    return result.data;
+  };
+
+  /// handleSubmit
+  const handleSubmit = async () => {
+    try {
+      const result = await login(loginForm.email, loginForm.password);
+      if (result && result.accessToken) {
+        console.log(result);
+        // 보안상의 이유로, 액세스 토큰을 로컬 스토리지 대신 쿠키 등에 저장하는 방법을 고려하세요.
+        localStorage.setItem("accessToken", result.accessToken);
+        navigate("/");
+      } else {
+        // 로그인 실패 처리
+        console.error("로그인 실패: 토큰을 받지 못했습니다.");
+      }
+    } catch (error) {
+      // 에러 핸들링
+      console.error("로그인 에러:", error);
+    }
+  };
+
   return (
     <div className="w-[520px] mx-auto h-[900px] pt-28">
       <div className="flex flex-col gap-16">
         <TItle tag="h2" className="w-full" text="이메일로 시작하기" />
-        <form onSubmit="" className="flex flex-col w-full">
+
+        <form onSubmit={handleSubmit} className="flex flex-col w-full">
           <Input
+            value={loginForm.email}
+            onChange={(e) =>
+              setLoginForm({ ...loginForm, email: e.target.value })
+            }
             type="email"
             text="이메일"
+            name="email"
+            pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
             placeholder="이메일 입력"
             className="mb-6 "
+            required
           />
-          <Input type="password" text="비밀번호" placeholder="비밀번호 입력" />
+          <Input
+            value={loginForm.password}
+            onChange={(e) =>
+              setLoginForm({ ...loginForm, password: e.target.value })
+            }
+            type="password"
+            text="비밀번호"
+            name="password"
+            placeholder="비밀번호 입력"
+            minLength="6"
+            required
+          />
         </form>
 
         <div className="flex flex-col items-center w-full ">
@@ -23,11 +76,13 @@ export default function SignInPage() {
             className="bg-gray-900 text-white mb-4 w-full"
             text="로그인"
           />
-          <div className="flex gap-4 font-light">
+          <div className="flex items-center gap-2 font-light">
             <p>계정이 없으신가요?</p>
 
             <Link to="/signup">
-              <p className="underline font-semibold">이메일로 회원가입</p>
+              <p className="underline text-lg font-semibold">
+                이메일로 회원가입
+              </p>
             </Link>
           </div>
         </div>
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,27 +1,64 @@
 import Button from "../components/Common/Button";
 import Input from "../components/Form/Input";
 import TItle from "../components/Common/Title";
+import { useState } from "react";
 
 export default function SignUpPage() {
+  const [value, setValue] = useState({
+    email: "",
+    password: "",
+    error: "",
+  });
+
+  ///회원가입 함수
+  const signup = async (email, password) => {
+    const result = await axios.post("", { email, password });
+    return result.data;
+  };
+
+  ///handleSignUpSubmit( 회원가입을 try 하고 catch 함)
+  const handleSignUpSubmit = async () => {
+    await signup(value.email, value.password);
+  };
+
   return (
     <div className="w-[520px] h-[900px] pt-28 mx-auto">
       <div className="flex flex-col items-center gap-16 ">
         <TItle tag="h2" className=" w-full" text="이메일로 회원가입" />
 
-        <form onSubmit="" className="flex flex-col w-full">
+        <form onSubmit={handleSignUpSubmit} className="flex flex-col w-full">
           <Input
+            value={value.email}
+            onChange={(e) => {
+              setValue({ ...value, email: e.target.value });
+            }}
             type="email"
             text="이메일"
             placeholder="이메일 입력"
             className="mb-6 "
+            pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
           />
           <Input
+            value={value.password}
+            onChange={(e) => {
+              setValue({ ...value, password: e.target.value });
+            }}
             type="password"
             text="비밀번호"
             placeholder="비밀번호 입력"
-            className="mb-6 "
+            className="mb-6"
+            minLength="6"
           />
-          <Input type="password" text="비밀번호" placeholder="비밀번호 확인" />
+          <Input
+            value={value.password}
+            onChange={(e) => {
+              setValue({ ...value, password: e.target.value });
+            }}
+            type="password"
+            text="비밀번호"
+            placeholder="비밀번호 확인"
+            minLength="6"
+          />
         </form>
 
         <div className="flex flex-col items-center w-full ">
