ce07626ca16226fe3694c8c9f76b1ffdbf8a319f
김민재 <kimminjae@gimminjaeui-MacBookPro.local>
2024-03-20 18:26:17 +0900

    feat:헤더 퍼블리싱

--- src/layout/Gnb.jsx
+++ src/layout/Gnb.jsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
+import Button from "../components/Common/Button";
 export default function Gnb() {
   return (
-    <header className="flex p-8 sticky top-0 w-full bg-white z-10 border-gray-200 border-solid border-2">
-      <Link to="/">
-        <h1>FE-MINI-2</h1>
-      </Link>
-      <nav>
-        <Link to="/list">ListPage</Link>
-        <Link to="/detail">DetailPage</Link>
-        <Link to="/signin">SignInPage</Link>
-        <Link to="/signup">SignUpPage</Link>
-      </nav>
+    <header className="py-8 sticky top-0 bg-white border-b-2 border-gray-200 border-solid z-10">
+      <div className="flex items-center justify-between gap-9 max-w-mw mx-auto">
+        <Link to="/">
+          <h1 className="text-primary text-3xl font-bold">FE-MINI-2</h1>
+        </Link>
+        <nav>
+          <Link to="/list">ListPage</Link>
+          <Link to="/detail">DetailPage</Link>
+          <Link to="/signin">
+            <Button text="로그인" />
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 }
--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,38 +1,37 @@
-import { Link } from 'react-router-dom';
-import Button from '../components/Common/Button.jsx';
-import TItle from '../components/Common/Title';
-import Input from '../components/Form/Input';
+import { Link } from "react-router-dom";
+import Button from "../components/Common/Button.jsx";
+import TItle from "../components/Common/Title";
+import Input from "../components/Form/Input";
 
 export default function SignInPage() {
-    return (
-        <div className="w-4/12 mx-auto">
-            <div className="flex flex-col items-center gap-16 ">
-                <TItle tag="h2" className=" w-full" text="이메일로 시작하기" />
-                <form onSubmit="" className="flex flex-col w-full">
-                    <Input
-                        type="email"
-                        text="아이디"
-                        placeholder="아이디 입력"
-                        className="mb-6 "
-                    />
-                    <Input
-                        type="password"
-                        text="비밀번호"
-                        placeholder="비밀번호 입력"
-                    />
-                </form>
+  return (
+    <div className="w-4/12 mx-auto">
+      <div className="flex flex-col items-center gap-16 ">
+        <TItle tag="h2" className=" w-full" text="이메일로 시작하기" />
+        <form onSubmit="" className="flex flex-col w-full">
+          <Input
+            type="email"
+            text="아이디"
+            placeholder="아이디 입력"
+            className="mb-6 "
+          />
+          <Input type="password" text="비밀번호" placeholder="비밀번호 입력" />
+        </form>
 
-                <div className="flex flex-col items-center w-full ">
-                    <Button
-                        className="bg-gray-900 text-white mb-4 w-full"
-                        text="로그인"
-                    />
-                    <div className="flex gap-4 font-light">
-                        <p>계정이 없으신가요?</p>
-                        <Link> 이메일로 회원가입</Link>
-                    </div>
-                </div>
-            </div>
+        <div className="flex flex-col items-center w-full ">
+          <Button
+            className="bg-gray-900 text-white mb-4 w-full"
+            text="로그인"
+          />
+          <div className="flex gap-4 font-light">
+            <p>계정이 없으신가요?</p>
+
+            <Link to="/signup">
+              <p className="underline font-semibold">이메일로 회원가입</p>
+            </Link>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
