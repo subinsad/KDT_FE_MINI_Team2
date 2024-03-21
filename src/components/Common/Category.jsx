612c92503995289062f69743542a51f163fb5880
subbi <92204014+subinsad@users.noreply.github.com>
2024-03-21 18:54:36 +0900

    test : 리스트배열 확인

--- src/components/Common/Category.jsx
+++ src/components/Common/Category.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function Category({ className, text }) {
+function Category({ className, text, ...props }) {
     return (
         <button
             className={`py-2 px-4 rounded border hover:bg-gray-800 hover:text-neutral-50
-                border-gray-600 text-gray-800 bg-white ${className}`}>
+                border-gray-600 text-gray-800 bg-white ${className}`}
+            {...props}>
             {text}
         </button>
     );
--- src/components/MainComponents/StayCategory.jsx
+++ src/components/MainComponents/StayCategory.jsx
@@ -7,7 +7,7 @@ function StayCategory() {
         <div className="flex gap-2">
             <Category text="전체" />
             <Category text="모텔" />
-            <Category text="호텔" />
+            <Category text="호텔" value="호텔" />
             <Category text="리조트" />
             <Category text="펜션" />
             <Category text="캠핑" />
