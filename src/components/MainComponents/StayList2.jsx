2cf1818a6f4f0e5ebfa25a33ad21ce34fa391338
subbi <92204014+subinsad@users.noreply.github.com>
2024-03-25 16:42:15 +0900

    feat : list 배열조건추가

--- src/components/MainComponents/StayList2.jsx
+++ src/components/MainComponents/StayList2.jsx
@@ -3,88 +3,42 @@ import TItle from '../Common/Title';
 import StayItem from '../StayItem';
 import PriceBlock from '../PriceBlock';
 
+import useStore from '../../store/accomodation';
+
 function StayList2() {
+    const { accomodation } = useStore();
+
     return (
         <div>
             <TItle tag="h2" text="특가 숙소" className="mb-4" />
-            <div className="flex gap-4 ">
-                <div className="flex flex-col gap-3 w-fit">
-                    <img
-                        src="../img/Frame4.png"
-                        alt="숙소이미지"
-                        className=" w-72 h-40 bg-slate-300 rounded"
-                    />
+            <ul className="flex gap-4 ">
+                <div className="flex gap-3 w-fit">
+                    {accomodation
+                        ?.sort((a, b) => b.sale - a.sale)
+                        .slice(0, 4)
+                        .map((item, index) => (
+                            <li key={index}>
+                                <img
+                                    src="../img/Frame4.png"
+                                    alt="숙소이미지"
+                                    className=" w-72 h-40 bg-slate-300 rounded"
+                                />
 
-                    <StayItem
-                        category="숙소 종류"
-                        star="10.0"
-                        stayTitle="숙소 이름"
-                        position="위치"
-                    />
-                    <PriceBlock
-                        text="-50%"
-                        fixedPrice="2,000,000원"
-                        discountPrice="1,000,000원"
-                    />
+                                <StayItem
+                                    category={item.cartegory}
+                                    star={item.star}
+                                    stayTitle={item.accomodation_name}
+                                    position={item.location_id.location_name}
+                                />
+                                <PriceBlock
+                                    text={`-${item.sale}%`}
+                                    fixedPrice={item.price}
+                                    discountRate={item.sale}
+                                />
+                            </li>
+                        ))}
                 </div>
-                <div className="flex flex-col gap-3 w-fit">
-                    <img
-                        src="../img/Frame4.png"
-                        alt="숙소이미지"
-                        className=" w-72 h-40 bg-slate-300 rounded"
-                    />
-
-                    <StayItem
-                        category="숙소 종류"
-                        star="10.0"
-                        stayTitle="숙소 이름"
-                        position="위치"
-                    />
-                    <PriceBlock
-                        text="-50%"
-                        fixedPrice="2,000,000원"
-                        discountPrice="1,000,000원"
-                    />
-                </div>
-                <div className="flex flex-col gap-3 w-fit">
-                    <img
-                        src="../img/Frame4.png"
-                        alt="숙소이미지"
-                        className=" w-72 h-40 bg-slate-300 rounded"
-                    />
-
-                    <StayItem
-                        category="숙소 종류"
-                        star="10.0"
-                        stayTitle="숙소 이름"
-                        position="위치"
-                    />
-                    <PriceBlock
-                        text="-50%"
-                        fixedPrice="2,000,000원"
-                        discountPrice="1,000,000원"
-                    />
-                </div>
-                <div className="flex flex-col gap-3 w-fit">
-                    <img
-                        src="../img/Frame4.png"
-                        alt="숙소이미지"
-                        className=" w-72 h-40 bg-slate-300 rounded"
-                    />
-
-                    <StayItem
-                        category="숙소 종류"
-                        star="10.0"
-                        stayTitle="숙소 이름"
-                        position="위치"
-                    />
-                    <PriceBlock
-                        text="-50%"
-                        fixedPrice="2,000,000원"
-                        discountPrice="1,000,000원"
-                    />
-                </div>
-            </div>
+            </ul>
         </div>
     );
 }
