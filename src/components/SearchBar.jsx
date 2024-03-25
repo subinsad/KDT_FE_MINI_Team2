526e922f223024040ec61411004f846b885e2b92
JeonYooDeok <jyd910215@gmail.com>
2024-03-25 11:56:16 +0900

    feat : 리스트페이지 필터기능, 검색결과 수 노출, 무한스크롤, 지역검색 추가

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,37 +1,44 @@
-import Input from './Form/Input';
+import Input from "./Form/Input";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Button from "./Common/Button";
 
-import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
+function SearchBar({ onSearch, className }) {
+  const [startDate, setStartDate] = useState(new Date()); //시작날짜
+  const [endDate, setEndDate] = useState(new Date()); //끝나는날짜
+  const [location, setLocation] = useState(""); // 지역명
 
-import 'react-datepicker/dist/react-datepicker.css';
-import Button from './Common/Button';
+  //함수작성
+  const onChange = (dates) => {
+    const [start, end] = dates;
+  };
 
-function SearchBar({ className }) {
-    const [startDate, setStartDate] = useState(new Date()); //시작날짜
-    const [endDate, setEndDate] = useState(new Date()); //끝나는날짜
+  const handleSearch = () => {
+    onSearch(location);
+  };
 
-    //함수작성
-    const onChange = (dates) => {
-        const [start, end] = dates;
-    };
-    return (
-        <div className={`flex  gap-5  p-6 bg-white rounded-2xl ${className}`}>
-            <Input placeholder="지역명 입력" className="w-80" />
-            <DatePicker
-                selectsRange={true} // 범위설정
-                selected={startDate}
-                onChange={(dates) => onChange(dates)}
-                startDate={startDate}
-                endDate={endDate}
-                minDate={new Date()} //지난날짜 비활성화
-                className="w-80 h-8 p-6 rounded bg-gray-1"
-            />
-
-            <Input placeholder="인원 입력" className="w-80 bg-gray-1" />
-
-            <Button text="검색" className="" />
-        </div>
-    );
+  return (
+    <div className={`flex  gap-5  p-6 bg-white rounded-2xl ${className}`}>
+      <Input
+        placeholder="지역명 입력"
+        className="w-80"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+      <DatePicker
+        selectsRange={true} // 범위설정
+        selected={startDate}
+        onChange={(dates) => onChange(dates)}
+        startDate={startDate}
+        endDate={endDate}
+        minDate={new Date()} //지난날짜 비활성화
+        className="w-80 h-8 p-6 rounded bg-gray-1"
+      />
+      <Input placeholder="인원 입력" className="w-80 bg-gray-1" />
+      <Button text="검색" onClick={handleSearch} className="" />
+    </div>
+  );
 }
 
 export default SearchBar;
