e24282a3dedaaf40bb64e330ef9136a4d6012e8d
subbi <92204014+subinsad@users.noreply.github.com>
2024-03-25 14:46:42 +0900

    feat : 별점순으로 나열, 처음페이지 전체로 보이게 나열

--- src/components/Common/Category.jsx
+++ src/components/Common/Category.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-
-function Category({ className, text, value, onClick, ...props }) {
+function Category({ className, text, value, onClick, isSelected, ...props }) {
     return (
         <button
-            className={`py-2 px-4 rounded border active:bg-gray-800 active:text-neutral-50
-                border-gray-600 text-gray-800 bg-white ${className}`}
+            className={`py-2 px-4 rounded border ${
+                isSelected ? 'active:bg-gray-800 active:text-neutral-50' : ''
+            } border-gray-600 text-gray-800 bg-white ${className}`}
             value={value}
             onClick={onClick}
             {...props}>
--- src/components/MainComponents/StayList1.jsx
+++ src/components/MainComponents/StayList1.jsx
@@ -10,89 +10,79 @@ import Category from '../Common/Category';
 
 function StayList1() {
     const { accomodation, ajax } = useStore(); // accomodation 상태와 ajax 함수 가져오기
-    const [filterItem, setFilterItem] = useState(accomodation); // 초기값은 'all'
+    const [filterItem, setFilterItem] = useState([]); // 초기값은 빈배열
+
+    const [selectedCategory, setSelectedCategory] = useState('all');
 
     useEffect(() => {
         // 페이지가 로드될 때 데이터 가져오기
-        ajax();
-        setFilterItem(accomodation);
-    }, []); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 호출되도록 함
+        ajax().then((data) => {
+            setFilterItem(data);
+        });
+    }, [ajax]); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 호출되도록 함
+
+    const itemsToDisplay = filterItem || accomodation || []; //둘중하나를 사용
+
+    const btnItem = [...new Set(accomodation?.map((item) => item.cartegory))]; //버튼 카테고리
 
     const handleBtn = (category) => {
+        setSelectedCategory(category);
         if (category === 'all') {
             setFilterItem(accomodation);
         } else {
-            const filterItem = accomodation.filter(
+            const filteredItems = accomodation.filter(
                 (item) => item.cartegory === category
             );
-            setFilterItem(filterItem);
+            setFilterItem(filteredItems);
         }
-        console.log(category);
     };
 
     return (
         <div className="flex flex-col gap-4">
             <TItle tag="h2" text="인기 숙소" />
+
             <div className="flex gap-2">
                 <Category
                     text="전체"
                     value="all"
                     onClick={() => handleBtn('all')}
                 />
-                <Category
-                    text="모텔"
-                    value="motel"
-                    onClick={() => handleBtn('모텔')}
-                />
-                <Category
-                    text="호텔"
-                    value="hotel"
-                    onClick={() => handleBtn('호텔')}
-                />
-                <Category
-                    text="리조트"
-                    value="resort"
-                    onClick={() => handleBtn('리조트')}
-                />
-                <Category
-                    text="펜션"
-                    value="pension"
-                    onClick={() => handleBtn('펜션')}
-                />
-                <Category
-                    text="캠핑"
-                    value="camping"
-                    onClick={() => handleBtn('캠핑')}
-                />
-                <Category
-                    text="게스트하우스"
-                    value="guestHouse"
-                    onClick={() => handleBtn('게스트하우스')}
-                />
+                {btnItem.map((category, index) => (
+                    <Category
+                        key={index}
+                        text={category}
+                        value={category}
+                        onClick={() => handleBtn(category)}
+                        isSelected={selectedCategory === category}
+                    />
+                ))}
             </div>
 
-            <ul className="flex gap-4 ">
+            <ul className="flex gap-4">
                 <div className="flex gap-3 w-fit">
-                    {filterItem.map((item, index) => (
-                        <li key={index} value={item.cartegory}>
-                            <img
-                                src="../img/Frame4.png"
-                                alt="숙소이미지"
-                                className=" w-72 h-40 bg-slate-300 rounded"
-                            />
-                            <StayItem
-                                category={item.cartegory}
-                                star={item.star}
-                                stayTitle={item.accomodation_name}
-                                position={item.location_id.location_name}
-                            />
-                            <PriceBlock
-                                text="-50%"
-                                fixedPrice={item.price}
-                                discountPrice="1,000,000원"
-                            />
-                        </li>
-                    ))}
+                    {itemsToDisplay
+                        ?.sort((a, b) => b.star - a.star)
+                        .slice(0, 4)
+                        .map((item, index) => (
+                            <li key={index} value={item.cartegory}>
+                                <img
+                                    src="../img/Frame4.png"
+                                    alt="숙소이미지"
+                                    className="w-72 h-40 bg-slate-300 rounded"
+                                />
+                                <StayItem
+                                    category={item.cartegory}
+                                    star={item.star}
+                                    stayTitle={item.accomodation_name}
+                                    position={item.location_id.location_name}
+                                />
+                                <PriceBlock
+                                    text="-50%"
+                                    fixedPrice={item.price}
+                                    discountPrice="1,000,000원"
+                                />
+                            </li>
+                        ))}
                 </div>
             </ul>
         </div>
