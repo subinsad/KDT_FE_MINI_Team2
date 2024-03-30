b14d42abc43a56ecc3a23d86fec6a98f76d18ae8
subbi <92204014+subinsad@users.noreply.github.com>
2024-03-30 17:13:33 +0900

    feat : 반복문으로 수정

--- src/components/MainComponents/CityList.jsx
+++ src/components/MainComponents/CityList.jsx
@@ -18,100 +18,51 @@ function CityList() {
         ajax();
     }, []); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 호출되도록 함
 
-    // 데이터가 로드되었는지 확인 후 jejuItem 찾기
-    const jejuItem = data && data.find((item) => item.locationType === 'JEJU');
-    // jejuItem이 존재하는 경우에만 jejuLink 설정
-    const jejuLink = jejuItem ? jejuItem.locationType : null;
+    const cities = [
+        'JEJU',
+        'SEOUL',
+        'BUSAN',
+        'GANGNEUNG',
+        'INCHEON',
+        'GYEONGJU',
+    ];
 
-    //서울
-    const seoulItem =
-        data && data.find((item) => item.locationType === 'SEOUL');
-    const seoulLink = seoulItem ? seoulItem.locationType : null;
+    const cityLinks = cities.map((city) => {
+        const cityItem =
+            data && data.find((item) => item.locationType === city);
+        const cityLink = cityItem ? cityItem.locationType : null;
+        return cityLink;
+    });
 
-    //부산
-    const busanItem =
-        data && data.find((item) => item.locationType === 'BUSAN');
-    const busanLink = busanItem ? busanItem.locationType : null;
+    const cityImg = {
+        JEJU: city1,
+        SEOUL: city2,
+        BUSAN: city3,
+        GANGNEUNG: city4,
+        INCHEON: city5,
+        GYEONGJU: city6,
+    };
 
-    //강릉
-    const gangneungItem =
-        data && data.find((item) => item.locationType === 'GANGNEUNG');
-    const gangneungLink = gangneungItem ? gangneungItem.locationType : null;
-
-    // 인천
-    const incheonItem =
-        data && data.find((item) => item.locationType === 'INCHEON');
-    const incheonLink = incheonItem ? incheonItem.locationType : null;
-
-    // 경주
-    const gyeongjuItem =
-        data && data.find((item) => item.locationType === 'GYEONGJU');
-    const gyeongjuLink = gyeongjuItem ? gyeongjuItem.locationType : null;
+    console.log(cityLinks);
 
     return (
         <div>
             <TItle tag="h2" text="인기 여행지" className="mb-4" />
 
             <div className="flex justify-between">
-                <Link to={`/list/${jejuLink}`} className="flex flex-col gap-2">
-                    <img
-                        src={city1}
-                        alt="이미지"
-                        className=" w-44 h-44 bg-slate-300 rounded"
-                    />
-                    <span className="text-sm text-gray-800 ">제주</span>
-                </Link>
-
-                <Link to={`/list/${seoulLink}`} className="flex flex-col gap-2">
-                    <img
-                        src={city2}
-                        alt="이미지"
-                        className=" w-44 h-44 bg-slate-300 rounded"
-                    />
-                    <span className="text-sm text-gray-800 "> 서울</span>
-                </Link>
-
-                <Link to={`/list/${busanLink}`} className="flex flex-col gap-2">
-                    <img
-                        src={city3}
-                        alt="이미지"
-                        className=" w-44 h-44 bg-slate-300 rounded"
-                    />
-                    <span className="text-sm text-gray-800 "> 부산</span>
-                </Link>
-
-                <Link
-                    to={`/list/${gangneungLink}`}
-                    className="flex flex-col gap-2">
-                    <img
-                        src={city4}
-                        alt="이미지"
-                        className=" w-44 h-44 bg-slate-300 rounded"
-                    />
-                    <span className="text-sm text-gray-800 "> 강릉</span>
-                </Link>
-
-                <Link
-                    to={`/list/${incheonLink}`}
-                    className="flex flex-col gap-2">
-                    <img
-                        src={city5}
-                        alt="이미지"
-                        className=" w-44 h-44 bg-slate-300 rounded"
-                    />
-                    <span className="text-sm text-gray-800 ">인천</span>
-                </Link>
-
-                <Link
-                    to={`/list/${gyeongjuLink}`}
-                    className="flex flex-col gap-2">
-                    <img
-                        src={city6}
-                        alt="이미지"
-                        className=" w-44 h-44 bg-slate-300 rounded"
-                    />
-                    <span className="text-sm text-gray-800 "> 경주</span>
-                </Link>
+                {cities.map((city, index) => (
+                    <Link
+                        key={index}
+                        to={`/list/${cityLinks[index]}`}
+                        className="flex flex-col gap-2">
+                        <img
+                            src={cityImg[city]}
+                            alt={`${city} 이미지`}
+                            className=" w-44 h-44 bg-slate-300 rounded"
+                        />
+                        <span className="text-sm text-gray-800 ">{city}</span>
+                    </Link>
+                ))}
             </div>
         </div>
     );
