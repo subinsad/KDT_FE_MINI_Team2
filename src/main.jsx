2eed32ca61c0fd6018cf9b988c06e15c8cad802d
subbi <92204014+subinsad@users.noreply.github.com>
2024-04-02 10:57:12 +0900

    feat : 공지사항 퍼블리싱, json연결

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import Main from "./pages/MainPage";
-import DetailPage from "./pages/DetailPage.jsx";
-import ListPage from "./pages/ListPage.jsx";
-import SignInPage from "./pages/SignInPage.jsx";
-import SignUpPage from "./pages/SignUpPage.jsx";
-import Reservation from "./pages/Reservation.jsx";
-import ReservationComplete from "./pages/ReservationComplete.jsx";
-import { CookiesProvider } from "react-cookie";
-import MyInfo from "./pages/MyInfo.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import Main from './pages/MainPage';
+import DetailPage from './pages/DetailPage.jsx';
+import ListPage from './pages/ListPage.jsx';
+import SignInPage from './pages/SignInPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import Reservation from './pages/Reservation.jsx';
+import ReservationComplete from './pages/ReservationComplete.jsx';
+import { CookiesProvider } from 'react-cookie';
+import MyInfo from './pages/MyInfo.jsx';
 
-import "./index.css";
+import './index.css';
+import Notice from './pages/Notice';
+import NoticeDetail from './pages/NoticeDetail';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, element: <Main /> },
-      { path: "/detail/:id", element: <DetailPage /> },
-      { path: "/list/:type?/:location?", element: <ListPage /> },
-      { path: "/signin", element: <SignInPage /> },
-      { path: "/signup", element: <SignUpPage /> },
-      {
-        path: "/reservation/:accomodation_id/:accomodation_name/:room_info",
-        element: <Reservation />,
-      },
-      { path: "/reservationcomplete", element: <ReservationComplete /> },
-      { path: "/myinfo", element: <MyInfo /> },
-    ],
-  },
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            { index: true, element: <Main /> },
+            { path: '/detail/:id', element: <DetailPage /> },
+            { path: '/list/:type?/:location?', element: <ListPage /> },
+            { path: '/signin', element: <SignInPage /> },
+            { path: '/signup', element: <SignUpPage /> },
+            {
+                path: '/reservation/:accomodation_id/:accomodation_name/:room_info',
+                element: <Reservation />,
+            },
+            { path: '/reservationcomplete', element: <ReservationComplete /> },
+            { path: '/myinfo', element: <MyInfo /> },
+            { path: '/notice', element: <Notice /> },
+            { path: '/notice/:id', element: <NoticeDetail /> },
+        ],
+    },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CookiesProvider>
-      <RouterProvider router={router} />
-    </CookiesProvider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <CookiesProvider>
+            <RouterProvider router={router} />
+        </CookiesProvider>
+    </React.StrictMode>
 );
---
+++ src/pages/NoticeDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NoticeDetail() {
+    return (
+        <>
+            <div>detail</div>
+        </>
+    );
+}
+
+export default NoticeDetail;
