8e25df8fa8075043b815dec813cba07895ca827a
JeonYooDeok <jyd910215@gmail.com>
2024-04-02 14:00:46 +0900

    feat : 슬라이드, 로고 교체

--- src/components/MainComponents/Banner.jsx
+++ src/components/MainComponents/Banner.jsx
@@ -1,35 +1,64 @@
-import React from 'react';
+import React from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
+import "swiper/css";
 
-import banner1 from '../../img/banner.png';
+import banner1 from "../../img/banner1.png";
+import banner2 from "../../img/banner2.png";
+import banner3 from "../../img/banner3.png";
+import banner4 from "../../img/banner4.png";
+import banner5 from "../../img/banner5.png";
 
 function Banner() {
-    return (
-        <Swiper
-            className="w-full h-[26rem] bg-slate-300 rounded"
-            spaceBetween={50}
-            slidesPerView={1}
-            loop={true}
-            navigation={true}
-            autoplay={{ delay: 3000 }}
-            pagination={{ clickable: true }} // 페이지네이션 설정
-        >
-            <SwiperSlide>
-                <img
-                    src={banner1}
-                    alt="Slide 1"
-                    className="w-full h-full object-cover"
-                />
-            </SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-        </Swiper>
-    );
+  return (
+    <Swiper
+      className="w-full h-[26rem] bg-slate-300 rounded"
+      spaceBetween={50}
+      slidesPerView={1}
+      loop={true}
+      navigation={true}
+      autoplay={{ delay: 3000 }}
+      pagination={{ clickable: true }} // 페이지네이션 설정
+    >
+      <SwiperSlide>
+        <img
+          src={banner1}
+          alt="Slide 1"
+          className="w-full h-full object-cover"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src={banner2}
+          alt="Slide 2"
+          className="w-full h-full object-cover"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src={banner3}
+          alt="Slide 3"
+          className="w-full h-full object-cover"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src={banner4}
+          alt="Slide 4"
+          className="w-full h-full object-cover"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src={banner5}
+          alt="Slide 5"
+          className="w-full h-full object-cover"
+        />
+      </SwiperSlide>
+    </Swiper>
+  );
 }
 
 export default Banner;
