5d3230857375fce9fb97c116a7471cd89b3f5882
subbi <92204014+subinsad@users.noreply.github.com>
2024-04-05 00:29:59 +0900

    style : 사진css수정

--- src/components/DetailComponents/DetailImage.jsx
+++ src/components/DetailComponents/DetailImage.jsx
@@ -7,35 +7,33 @@ export default function DetailImage({ roomData, detailItem }) {
                 <img
                     src={detailItem?.accommodationImage[0]?.imagePath}
                     alt="객실이미지"
-                    className="w-[656px] h-full rounded-xl border-gray-200 border-solid border-2 p-4"
+                    className="w-[656px] h-full rounded-xl "
                 />
             )}
 
             <div className="grid grow gap-2 grid-rows-2 grid-cols-2">
-                {roomImg.length > 0 && roomImg[1] && (
-                    <img
-                        src={roomImg[1]?.imagePath}
-                        alt="객실이미지"
-                        className="h-full rounded-xl border-gray-200 border-solid border-2 p-4"
-                    />
-                )}
+                <img
+                    src={roomImg[1]?.imagePath}
+                    alt="객실이미지"
+                    className="h-full rounded-xl"
+                />
 
                 <img
                     src={roomImg[2]?.imagePath}
                     alt="객실이미지"
-                    className=" rounded-xl border-gray-200 border-solid border-2 p-4"
+                    className=" rounded-xl "
                 />
 
                 <img
                     src={roomImg[3]?.imagePath}
                     alt="객실이미지"
-                    className=" rounded-xl border-gray-200 border-solid border-2 p-4"
+                    className=" rounded-xl "
                 />
 
                 <img
                     src={roomImg[4]?.imagePath}
                     alt="객실이미지"
-                    className=" rounded-xl border-gray-200 border-solid border-2 p-4"
+                    className=" rounded-xl "
                 />
             </div>
         </div>
