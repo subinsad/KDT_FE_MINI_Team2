23e38a47640cba3436c2209efc74c7d1d605b7bb
김민재 <kimminjae@gimminjaeui-MacBookPro.local>
2024-04-04 21:34:57 +0900

    push

--- src/components/DetailComponents/MapNavi.jsx
+++ src/components/DetailComponents/MapNavi.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import Button from '../Common/Button';
+import React from "react";
 
 export default function MapNavi({ moveBtn, bottomBtn }) {
-    return (
-        <div className="mb-3 pb-3 border-gray-200 border-solid border-b-2">
-            <button
-                className="mr-2 px-5 bg-white text-black "
-                onClick={moveBtn}>
-                객실
-            </button>
-            <button
-                className="px-5 bg-white text-stone-400 hover:text-black"
-                onClick={bottomBtn}>
-                위치
-            </button>
-        </div>
-    );
+  return (
+    <div className="mb-3 pb-3 border-gray-200 border-solid border-b-2">
+      <button className="mr-2 px-5 bg-white text-black " onClick={moveBtn}>
+        객실
+      </button>
+      <button
+        className="px-5 bg-white text-stone-400 hover:text-black"
+        onClick={bottomBtn}
+      >
+        위치
+      </button>
+    </div>
+  );
 }
