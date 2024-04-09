7dd727553ceb1df77bc03609f003355f6876f18b
JeonYooDeok <jyd910215@gmail.com>
2024-04-09 18:23:11 +0900

    feat : 공지사항 관리자 관련 퍼블리싱

--- src/pages/NoticeDetail.jsx
+++ src/pages/NoticeDetail.jsx
@@ -1,46 +1,55 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import useStore from '../store/notice';
-import { GrLinkPrevious } from 'react-icons/gr';
+import React, { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import useStore from "../store/notice";
+import { GrLinkPrevious } from "react-icons/gr";
 
 function NoticeDetail() {
-    const { id } = useParams();
-    const { notice, ajax } = useStore();
-    const noticeItem = notice.find((item) => item.id === id);
+  const { id } = useParams();
+  const { notice, ajax } = useStore();
+  const noticeItem = notice.find((item) => item.id === id);
 
-    useEffect(() => {
-        ajax();
-    }, []);
+  useEffect(() => {
+    ajax();
+  }, []);
 
-    const title = noticeItem?.title || '';
-    const date = noticeItem?.date || '';
-    const text = noticeItem?.text || '';
+  const title = noticeItem?.title || "";
+  const date = noticeItem?.date || "";
+  const text = noticeItem?.text || "";
 
-    const handlBack = () => {
-        window.history.go(-1);
-    };
+  const handlBack = () => {
+    window.history.go(-1);
+  };
 
-    return (
-        <div className="max-w-mw mx-auto pt-12 pb-12">
-            <GrLinkPrevious
-                onClick={handlBack}
-                className="cursor-pointer mb-4"
-            />
-            <div className="flex flex-col gap-1 pb-4 mb-6 border-b-2 border-solid border-gray-300">
-                <b className="text-lg font-semibold"> {title}</b>
-                <p className="text-gray-500"> {date}</p>
-            </div>
-
-            <div className="h-[400px]">
-                {text.split('\n').map((line, index) => (
-                    <span key={index} className="text-gray-600 leading-10">
-                        {line}
-                        <br />
-                    </span>
-                ))}
-            </div>
+  return (
+    <div className="max-w-mw mx-auto pt-12 pb-12">
+      <GrLinkPrevious onClick={handlBack} className="cursor-pointer mb-4" />
+      <div className="flex flex-col gap-1 pb-4 mb-6 border-b-2 border-solid border-gray-300">
+        <b className="text-lg font-semibold"> {title}</b>
+        <div className="flex gap-1">
+          <span className="text-gray-500"> {date}</span>
+          <span className="text-gray-300">|</span>
+          <Link to={"/edit"}>
+            <span className="text-gray-500 cursor-pointer hover:text-black">
+              수정
+            </span>
+          </Link>
+          <span className="text-gray-300">|</span>
+          <span className="text-gray-500 cursor-pointer hover:text-black">
+            삭제
+          </span>
         </div>
-    );
+      </div>
+
+      <div className="h-[400px]">
+        {text.split("\n").map((line, index) => (
+          <span key={index} className="text-gray-600 leading-10">
+            {line}
+            <br />
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default NoticeDetail;
---
+++ src/pages/NoticeEdit.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Title from "../components/Common/Title";
+import Input from "../components/Form/Input";
+import Button from "../components/Common/Button";
+
+function NoticeEdit() {
+  return (
+    <div className="max-w-mw mt-28 mx-auto">
+      <div className="flex flex-col items-center gap-16 ">
+        <Title tag="h2" className=" w-full" text="글 작성" />
+        <form className="flex flex-col w-full">
+          <div className="flex flex-col w-full mb-6">
+            <Input type="text" text="제목" placeholder="제목 입력" />
+          </div>
+
+          <Button
+            className={`bg-gray-900 text-white mt-10 w-full `}
+            text={"확인"}
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default NoticeEdit;
