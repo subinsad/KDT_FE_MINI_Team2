fdd374437db3ac2126ec00754bb382eaa82a6025
JeonYooDeok <jyd910215@gmail.com>
2024-04-09 18:24:19 +0900

    feat : tailwind 분기점 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{js,jsx}'],
-    theme: {
-        extend: {
-            maxWidth: {
-                mw: '1200px',
-                //minWidth: ???
-            },
-            colors: {
-                primary: {
-                    DEFAULT: '#2393D2',
-                    1: '#E5E5E5',
-                },
-                gray: {
-                    DEFAULT: '#FFFFFF',
-                    1: '#F7F7F7',
-                    2: '#AEB0B4',
-                },
-            },
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      maxWidth: {
+        mw: "1200px",
+        //minWidth: ???
+      },
+      screens: {
+        samll: "320px",
+        medium: "600px",
+        large: "1024px",
+        xlarge: "1280px",
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#2393D2",
+          1: "#E5E5E5",
         },
+        gray: {
+          DEFAULT: "#FFFFFF",
+          1: "#F7F7F7",
+          2: "#AEB0B4",
+        },
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
