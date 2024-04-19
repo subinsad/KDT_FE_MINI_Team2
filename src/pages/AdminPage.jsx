a8e01ce42319e349a54b4fd53cee962640179f09
subbi <92204014+subinsad@users.noreply.github.com>
2024-04-19 11:16:01 +0900

    feat : 조회api연결

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,16 +1,42 @@
 // AdminPage.jsx
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
 
 import RoomItem from '../components/RegisterComponents/RoomItem';
 import TitleBtn from '../components/RegisterComponents/TitleBtn';
 import TItle from '../components/Common/Title';
 import { FaSearch } from 'react-icons/fa';
+import axios from 'axios';
 
 function AdminPage() {
     const [selectedRoomId, setSelectedRoomId] = useState(null);
-
+    const [cookies] = useCookies(['secretKey']);
     const handleRoomClick = (roomId) => {
         setSelectedRoomId(roomId);
+    };
+    const [accommodations, setAccommodations] = useState([]);
+
+    useEffect(() => {
+        fetchAccommodation();
+    }, []);
+
+    const fetchAccommodation = async () => {
+        try {
+            const response = await axios.get('/api/v1/accommodation/member', {
+                headers: {
+                    Authorization: `Bearer ${cookies.secretKey}`,
+                },
+            });
+
+            const responseData = response.data;
+
+            // 가져온 데이터에서 숙소 정보만 추출하여 상태에 업데이트
+            const accommodationsData = responseData.data || []; // 숙소 정보 배열
+            setAccommodations(accommodationsData);
+            console.log(response);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
     };
 
     return (
@@ -25,28 +51,19 @@ function AdminPage() {
                             className="mb-6"
                         />
                         <div className="flex justify-between flex-col p-8 border-2 border-solid border-gray-300 rounded-xl">
-                            <RoomItem
-                                key="1"
-                                id="1"
-                                title="서울호텔"
-                                text="1"
-                                room="3"
-                                type="호텔"
-                                btn="true"
-                                istrue={selectedRoomId === '1'} // 선택된 방일 때만 true
-                                onClick={() => handleRoomClick('1')} // 클릭 시 해당 방의 ID를 전달
-                            />
-                            <RoomItem
-                                key="2"
-                                id="2"
-                                title="인천호텔"
-                                text="2"
-                                room="5"
-                                type="호텔"
-                                btn="true"
-                                istrue={selectedRoomId === '2'}
-                                onClick={() => handleRoomClick('2')}
-                            />
+                            {accommodations.map((item, index) => (
+                                <RoomItem
+                                    key={index}
+                                    id={item.id}
+                                    title={item.accommodationName}
+                                    text={item.id}
+                                    room={item.room.index}
+                                    type={item.accommodationType}
+                                    btn="true"
+                                    istrue={selectedRoomId === item.id}
+                                    onClick={() => handleRoomClick(item.id)}
+                                />
+                            ))}
                         </div>
                     </div>
 
