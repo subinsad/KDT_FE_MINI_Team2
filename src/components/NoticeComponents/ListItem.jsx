1a72bb0cb9398ca5c849557f9919226d1085c713
JeonYooDeok <jyd910215@gmail.com>
2024-04-19 15:06:08 +0900

    feat : 스켈레톤 추가

--- src/components/NoticeComponents/ListItem.jsx
+++ src/components/NoticeComponents/ListItem.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function ListItem({ title, text, date, link }) {
-    return (
-        <Link to={link}>
-            <li className="flex flex-col gap-2 pt-4 pb-4 border-b-2 border-solid border-gray-300 large:flex-row large:justify-between large:items-center">
-                {/* <li className="flex justify-between items-center pt-4 pb-4 border-b-2 border-solid border-gray-300"> */}
-                <p className="text-lg font-semibold"> {title} </p>
-                <p className="text-gray-500">{date} </p>
-            </li>
-        </Link>
-    );
+  return (
+    <Link to={link}>
+      <li className="flex flex-col gap-2 pt-4 pb-4 border-b-2 border-solid border-gray-300 large:flex-row large:justify-between large:items-center">
+        {/* <li className="flex justify-between items-center pt-4 pb-4 border-b-2 border-solid border-gray-300"> */}
+        <p className="text-lg font-semibold"> {title} </p>
+        <p className="text-gray-500">{date} </p>
+      </li>
+    </Link>
+  );
 }
 
 export default ListItem;
