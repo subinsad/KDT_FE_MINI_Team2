131a16a6211ada4395a57c18e172762b7730fc80
subbi <92204014+subinsad@users.noreply.github.com>
2024-04-25 23:17:47 +0900

    feat : 페이지네이션, 모달창 추가

--- src/components/RegisterComponents/AccommodationItem.jsx
+++ src/components/RegisterComponents/AccommodationItem.jsx
@@ -1,84 +1,122 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { GrFormNext } from "react-icons/gr";
-import { Link } from "react-router-dom";
-import Button from "../Common/Button";
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { IoIosMore } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+import Button from '../Common/Button';
 
 function AccommodationItem({
-  title,
-  text,
-  room,
-  type,
-  onClick,
-  btn,
-  id,
-  setAccommodations,
-  cookies,
+    title,
+    text,
+    room,
+    type,
+    onClick,
+    btn,
+    id,
+    setAccommodations,
+    cookies,
+    fetchAccommodation,
 }) {
-  const [isDeleted, setIsDeleted] = useState(false);
-  // 삭제
-  const deleteBtn = async () => {
-    console.log(id);
-    try {
-      const response = await axios.delete(
-        `/api/v1/accommodation/${id}/delete`,
-        {
-          headers: {
-            Authorization: `Bearer ${cookies.secretKey}`,
-          },
+    const [isDeleted, setIsDeleted] = useState(false);
+    const [detail, setDetail] = useState(false);
+    const detailRef = useRef(null);
+
+    // 클릭 박스
+    const clickBox = () => {
+        setDetail(!detail);
+    };
+
+    // 클릭된 위치가 상세 정보 바깥쪽인지 확인하는 함수
+    const handleClickOutside = (event) => {
+        if (detailRef.current && !detailRef.current.contains(event.target)) {
+            setDetail(false); // 상세 정보를 닫음
         }
-      );
-      const responseData = response.data;
-      // 숙소 정보 업데이트
-      setAccommodations(responseData.data || []);
+    };
 
-      setIsDeleted(true);
-      console.log(isDeleted);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
+    useEffect(() => {
+        // 페이지가 로드될 때마다 클릭된 위치를 감지하여 상세 정보를 닫음
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
-  return isDeleted ? null : (
-    <ul>
-      <li className="gap-4 pt-4 pb-4 medium:pt-5 medium:pb-5">
-        <div
-          onClick={onClick}
-          className="flex flex-col gap-4 hover:cursor-pointer medium:gap-2"
-        >
-          <p className="text-xl font-bold"> {title} </p>
-          <div className="flex flex-col gap-4 medium:flex-row medium:items-center medium:justify-between">
-            <div className="flex gap-4">
-              <p className="text-gray-500">id : {text} </p>
-              <p className="text-gray-500">타입 : {type} </p>
-              <p className="text-gray-500">등록 룸 수 : {room}</p>
-            </div>
-            <div className="flex gap-2">
-              <button
-                text="삭제"
-                className="p-2 px-3 bg-red-100 rounded text-red-500 text-sm font-bold"
-                onClick={deleteBtn}
-              >
-                {" "}
-                삭제
-              </button>
-              {btn && (
-                <>
-                  <Link
-                    className="p-2 bg-gray-500 rounded text-white text-sm font-bold"
-                    to={`roomregister/${id}`}
-                  >
-                    {" "}
-                    + 룸 등록{" "}
-                  </Link>
-                </>
-              )}
-            </div>
-          </div>
-        </div>
-      </li>
-    </ul>
-  );
+    // 삭제
+    const deleteBtn = async () => {
+        try {
+            const response = await axios.delete(
+                `/api/v1/accommodation/${id}/delete`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${cookies.secretKey}`,
+                    },
+                }
+            );
+            const responseData = response.data;
+            // 숙소 정보 업데이트
+            setAccommodations(responseData.data || []);
+            fetchAccommodation();
+            setIsDeleted(true);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    // 삭제 확인
+    const confirmDelete = () => {
+        const confirmDelete = window.confirm('정말로 삭제하시겠습니까?');
+        if (confirmDelete) {
+            deleteBtn();
+        }
+    };
+
+    return isDeleted ? null : (
+        <ul>
+            <li className="gap-4 pt-4 pb-4 medium:pt-5 medium:pb-5">
+                <div
+                    onClick={onClick}
+                    className="flex flex-col gap-4 hover:cursor-pointer medium:gap-3">
+                    <div className="flex justify-between">
+                        <p className="text-xl font-bold"> {title} </p>
+                        <div className="flex flex-col items-end relative">
+                            <IoIosMore
+                                onClick={clickBox}
+                                className="cursor-pointer"
+                            />
+                            {detail && (
+                                <div
+                                    ref={detailRef}
+                                    className="absolute right-0 mt-2 w-40 bg-white border border-gray-300 rounded-md shadow-lg">
+                                    <ul className="py-1">
+                                        <li>
+                                            <button
+                                                className="block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                                onClick={confirmDelete}>
+                                                삭제
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <Link
+                                                to={`roomregister/${id}`}
+                                                className="block w-full px-4 py-2 text-sm text-center text-gray-700  hover:bg-gray-100">
+                                                + 룸 등록
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-4 medium:flex-row medium:items-center medium:justify-between">
+                        <div className="flex gap-4">
+                            <p className="text-gray-500">id : {text} </p>
+                            <p className="text-gray-500">타입 : {type} </p>
+                            <p className="text-gray-500">등록 룸 수 : {room}</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    );
 }
 
 export default AccommodationItem;
--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,99 +1,101 @@
-import { Link, useNavigate } from "react-router-dom";
-import Button from "../components/Common/Button.jsx";
-import Title from "../components/Common/Title";
-import Input from "../components/Form/Input";
-import { useState } from "react";
-import axios from "axios";
-import { useCookies } from "react-cookie";
-import { useUser } from "../store/user.js";
-import Spinner from "../components/Common/Spinner.jsx";
+import { Link, useNavigate } from 'react-router-dom';
+import Button from '../components/Common/Button.jsx';
+import Title from '../components/Common/Title';
+import Input from '../components/Form/Input';
+import { useState } from 'react';
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
+import { useUser } from '../store/user.js';
+import Spinner from '../components/Common/Spinner.jsx';
 
 export default function SignInPage() {
-  const [loginForm, setLoginForm] = useState({
-    email: "",
-    password: "",
-  });
-  const [message, setMessage] = useState("");
-  const [, setCookie] = useCookies(["secretKey"]);
-  const { setLoginUser, setMemberId } = useUser();
-  const [isLoading, setIsLoading] = useState(false);
+    const [loginForm, setLoginForm] = useState({
+        email: '',
+        password: '',
+    });
+    const [message, setMessage] = useState('');
+    const [, setCookie] = useCookies(['secretKey']);
+    const { setLoginUser, setMemberId } = useUser();
+    console.log(useUser);
+    const [isLoading, setIsLoading] = useState(false);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const loginHandler = async (e) => {
-    e.preventDefault();
-    setIsLoading(true); // 로그인 프로세스 시작, 스피너 활성화
-    try {
-      const response = await axios.post(
-        "/public-api/v1/member/login",
-        loginForm
-      );
-      const { secretKey, memberId } = response.data.data;
-      console.log(secretKey, memberId);
-      setCookie("secretKey", secretKey, { path: "/" });
-      setCookie("memberId", memberId, { path: "/" });
-      setLoginUser({ secretKey });
-      setMemberId({ memberId });
-      navigate("/");
-    } catch (error) {
-      setMessage("아이디 또는 비밀번호가 틀렸습니다.");
-    } finally {
-      setIsLoading(false);
-    }
-  };
+    const loginHandler = async (e) => {
+        e.preventDefault();
+        setIsLoading(true); // 로그인 프로세스 시작, 스피너 활성화
+        try {
+            const response = await axios.post(
+                '/public-api/v1/member/login',
+                loginForm
+            );
+            const { secretKey, memberId } = response.data.data;
+            console.log(secretKey, memberId);
+            setCookie('secretKey', secretKey, { path: '/' });
+            setCookie('memberId', memberId, { path: '/' });
+            setLoginUser({ secretKey });
 
-  return (
-    <div className="sm:w-[520px] mx-auto mt-28 px-5 xl:px-0">
-      <div className="flex flex-col gap-16">
-        <Title tag="h2" className="w-full" text="이메일로 시작하기" />
+            setMemberId({ memberId });
+            navigate('/');
+        } catch (error) {
+            setMessage('아이디 또는 비밀번호가 틀렸습니다.');
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
-        <form onSubmit={loginHandler} className="flex flex-col w-full">
-          <Input
-            value={loginForm.email}
-            onChange={(e) =>
-              setLoginForm({
-                ...loginForm,
-                email: e.target.value,
-              })
-            }
-            type="email"
-            text="이메일"
-            name="email"
-            placeholder="이메일 입력"
-            className="mb-6"
-            required
-          />
-          <Input
-            value={loginForm.password}
-            onChange={(e) =>
-              setLoginForm({
-                ...loginForm,
-                password: e.target.value,
-              })
-            }
-            type="password"
-            text="비밀번호"
-            name="password"
-            placeholder="비밀번호 입력"
-            required
-          />
-          {message && <p className="text-red-500 mt-3">{message}</p>}
-          <div className="flex flex-col items-center w-full ">
-            <Button
-              className="bg-gray-900 text-white mt-16 mb-4 w-full"
-              text={isLoading ? <Spinner /> : "로그인"}
-            />
-            <div className="flex items-center gap-2 font-light">
-              <p>계정이 없으신가요?</p>
-              <Link to="/signup">
-                <p className="underline text-lg font-semibold">
-                  이메일로 회원가입
-                </p>
-              </Link>
+    return (
+        <div className="sm:w-[520px] mx-auto mt-28 px-5 xl:px-0">
+            <div className="flex flex-col gap-16">
+                <Title tag="h2" className="w-full" text="이메일로 시작하기" />
+
+                <form onSubmit={loginHandler} className="flex flex-col w-full">
+                    <Input
+                        value={loginForm.email}
+                        onChange={(e) =>
+                            setLoginForm({
+                                ...loginForm,
+                                email: e.target.value,
+                            })
+                        }
+                        type="email"
+                        text="이메일"
+                        name="email"
+                        placeholder="이메일 입력"
+                        className="mb-6"
+                        required
+                    />
+                    <Input
+                        value={loginForm.password}
+                        onChange={(e) =>
+                            setLoginForm({
+                                ...loginForm,
+                                password: e.target.value,
+                            })
+                        }
+                        type="password"
+                        text="비밀번호"
+                        name="password"
+                        placeholder="비밀번호 입력"
+                        required
+                    />
+                    {message && <p className="text-red-500 mt-3">{message}</p>}
+                    <div className="flex flex-col items-center w-full ">
+                        <Button
+                            className="bg-gray-900 text-white mt-16 mb-4 w-full"
+                            text={isLoading ? <Spinner /> : '로그인'}
+                        />
+                        <div className="flex items-center gap-2 font-light">
+                            <p>계정이 없으신가요?</p>
+                            <Link to="/signup">
+                                <p className="underline text-lg font-semibold">
+                                    이메일로 회원가입
+                                </p>
+                            </Link>
+                        </div>
+                    </div>
+                </form>
             </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
