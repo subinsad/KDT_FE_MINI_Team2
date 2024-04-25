b53eb513969dc328db8a6f528599cb802b206b62
JeonYooDeok <jyd910215@gmail.com>
2024-04-25 10:40:21 +0900

    style : 숙소등록 스타일 수정

--- src/components/RegisterComponents/AccommodationItem.jsx
+++ src/components/RegisterComponents/AccommodationItem.jsx
@@ -1,76 +1,84 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { GrFormNext } from 'react-icons/gr';
-import { Link } from 'react-router-dom';
-import Button from '../Common/Button';
+import React, { useState } from "react";
+import axios from "axios";
+import { GrFormNext } from "react-icons/gr";
+import { Link } from "react-router-dom";
+import Button from "../Common/Button";
 
 function AccommodationItem({
-    title,
-    text,
-    room,
-    type,
-    onClick,
-    btn,
-    id,
-    setAccommodations,
-    cookies,
+  title,
+  text,
+  room,
+  type,
+  onClick,
+  btn,
+  id,
+  setAccommodations,
+  cookies,
 }) {
-    const [isDeleted, setIsDeleted] = useState(false);
-    // 삭제
-    const deleteBtn = async () => {
-        console.log(id);
-        try {
-            const response = await axios.delete(
-                `/api/v1/accommodation/${id}/delete`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${cookies.secretKey}`,
-                    },
-                }
-            );
-            const responseData = response.data;
-            // 숙소 정보 업데이트
-            setAccommodations(responseData.data || []);
-
-            setIsDeleted(true);
-            console.log(isDeleted);
-        } catch (error) {
-            console.error('Error fetching data:', error);
+  const [isDeleted, setIsDeleted] = useState(false);
+  // 삭제
+  const deleteBtn = async () => {
+    console.log(id);
+    try {
+      const response = await axios.delete(
+        `/api/v1/accommodation/${id}/delete`,
+        {
+          headers: {
+            Authorization: `Bearer ${cookies.secretKey}`,
+          },
         }
-    };
+      );
+      const responseData = response.data;
+      // 숙소 정보 업데이트
+      setAccommodations(responseData.data || []);
 
-    return isDeleted ? null : (
-        <ul>
-            <li className="flex justify-evenly items-end pt-4 pb-4 ">
-                <div onClick={onClick} className="hover:cursor-pointer">
-                    <p className="text-xl font-bold"> {title} </p>
-                    <div className="flex items-center gap-4">
-                        <p className="text-gray-500"> id : {text} </p>
-                        <p className="text-gray-500"> 숙소유형 : {type} </p>
-                        <p className="text-gray-500">등록된 룸 갯수 : {room}</p>
-                        <button
-                            text="삭제"
-                            className="p-2 px-3 bg-red-100 rounded text-red-500 text-sm font-bold"
-                            onClick={deleteBtn}>
-                            {' '}
-                            삭제
-                        </button>
-                    </div>
-                </div>
+      setIsDeleted(true);
+      console.log(isDeleted);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-                {btn && (
-                    <>
-                        <Link
-                            className="p-2 bg-gray-500 rounded text-white text-sm font-bold"
-                            to={`roomregister/${id}`}>
-                            {' '}
-                            + 룸 등록{' '}
-                        </Link>
-                    </>
-                )}
-            </li>
-        </ul>
-    );
+  return isDeleted ? null : (
+    <ul>
+      <li className="gap-4 pt-4 pb-4 medium:pt-5 medium:pb-5">
+        <div
+          onClick={onClick}
+          className="flex flex-col gap-4 hover:cursor-pointer medium:gap-2"
+        >
+          <p className="text-xl font-bold"> {title} </p>
+          <div className="flex flex-col gap-4 medium:flex-row medium:items-center medium:justify-between">
+            <div className="flex gap-4">
+              <p className="text-gray-500">id : {text} </p>
+              <p className="text-gray-500">타입 : {type} </p>
+              <p className="text-gray-500">등록 룸 수 : {room}</p>
+            </div>
+            <div className="flex gap-2">
+              <button
+                text="삭제"
+                className="p-2 px-3 bg-red-100 rounded text-red-500 text-sm font-bold"
+                onClick={deleteBtn}
+              >
+                {" "}
+                삭제
+              </button>
+              {btn && (
+                <>
+                  <Link
+                    className="p-2 bg-gray-500 rounded text-white text-sm font-bold"
+                    to={`roomregister/${id}`}
+                  >
+                    {" "}
+                    + 룸 등록{" "}
+                  </Link>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  );
 }
 
 export default AccommodationItem;
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,186 +1,179 @@
-import React, { useEffect, useState } from 'react';
-import { useCookies } from 'react-cookie';
-import { ScaleLoader } from 'react-spinners';
-import useStore from '../store/accomodation';
+import React, { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import { ScaleLoader } from "react-spinners";
+import useStore from "../store/accomodation";
 
-import RoomItem from '../components/RegisterComponents/RoomItem';
-import TitleBtn from '../components/RegisterComponents/TitleBtn';
-import TItle from '../components/Common/Title';
-import { FaSearch } from 'react-icons/fa';
-import axios from 'axios';
-import Pagination from '../components/Pagination';
-import AccommodationItem from '../components/RegisterComponents/AccommodationItem';
+import RoomItem from "../components/RegisterComponents/RoomItem";
+import TitleBtn from "../components/RegisterComponents/TitleBtn";
+import TItle from "../components/Common/Title";
+import { FaSearch } from "react-icons/fa";
+import axios from "axios";
+import Pagination from "../components/Pagination";
+import AccommodationItem from "../components/RegisterComponents/AccommodationItem";
 
 function AdminPage() {
-    const [selectedRoomId, setSelectedRoomId] = useState(null);
-    const [selectedRoom, setSelectedRoom] = useState([]); // 선택된 방 정보를 배열로 변경
+  const [selectedRoomId, setSelectedRoomId] = useState(null);
+  const [selectedRoom, setSelectedRoom] = useState([]); // 선택된 방 정보를 배열로 변경
 
-    const [cookies] = useCookies(['secretKey']);
-    const { data, ajax } = useStore(); // useStore로 전체 숙소 리스트 가져오기
-    const [accommodations, setAccommodations] = useState([]);
-    const [currentPage, setCurrentPage] = useState(0); // 현재 페이지 상태 설정
-    const [isLoading, setIsLoading] = useState(true);
+  const [cookies] = useCookies(["secretKey"]);
+  const { data, ajax } = useStore(); // useStore로 전체 숙소 리스트 가져오기
+  const [accommodations, setAccommodations] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0); // 현재 페이지 상태 설정
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        fetchAccommodation();
-    }, [currentPage]);
+  useEffect(() => {
+    fetchAccommodation();
+  }, [currentPage]);
 
-    //숙소전체데이터
-    useEffect(() => {
-        ajax();
-    }, []);
+  //숙소전체데이터
+  useEffect(() => {
+    ajax();
+  }, []);
 
-    useEffect(() => {
-        setSelectedRoom(data.filter((item) => item.id === selectedRoomId));
-    }, [selectedRoomId, data]);
+  useEffect(() => {
+    setSelectedRoom(data.filter((item) => item.id === selectedRoomId));
+  }, [selectedRoomId, data]);
 
-    const fetchAccommodation = async () => {
-        try {
-            setIsLoading(true);
-            const response = await axios.get('/api/v1/accommodation/member', {
-                headers: {
-                    Authorization: `Bearer ${cookies.secretKey}`,
-                },
-                params: {
-                    page: currentPage,
-                },
-            });
+  const fetchAccommodation = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get("/api/v1/accommodation/member", {
+        headers: {
+          Authorization: `Bearer ${cookies.secretKey}`,
+        },
+        params: {
+          page: currentPage,
+        },
+      });
 
-            const responseData = response.data;
-            // 가져온 데이터에서 숙소 정보만 추출하여 상태에 업데이트
-            const accommodationsData = responseData.data.result || []; // 숙소 정보 배열
-            setAccommodations(accommodationsData);
+      const responseData = response.data;
+      // 가져온 데이터에서 숙소 정보만 추출하여 상태에 업데이트
+      const accommodationsData = responseData.data.result || []; // 숙소 정보 배열
+      setAccommodations(accommodationsData);
 
-            if (accommodationsData.length === 0) {
-                setCurrentPage(0);
-            }
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        } finally {
-            setIsLoading(false);
+      if (accommodationsData.length === 0) {
+        setCurrentPage(0);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleRoomClick = async (roomId) => {
+    // 클릭 시 선택된 방 ID를 설정
+    setSelectedRoomId(roomId);
+
+    // 선택된 방 ID를 기반으로 숙소 정보를 다시 가져옴
+    await ajax();
+    fetchAccommodation();
+  };
+
+  //페이지네이션
+  const handlePrevious = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1); // 이전 페이지로 이동
+    }
+  };
+  const handleNext = async () => {
+    try {
+      const response = await axios.get(
+        `/v1/accommodation/member?page=${currentPage}`,
+        {
+          params: {
+            page: currentPage + 1, // 다음 페이지 번호 설정
+          },
         }
-    };
+      );
+      console.log(response);
+      setCurrentPage(currentPage + 1); // 페이지 번호 업데이트
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-    const handleRoomClick = async (roomId) => {
-        // 클릭 시 선택된 방 ID를 설정
-        setSelectedRoomId(roomId);
-
-        // 선택된 방 ID를 기반으로 숙소 정보를 다시 가져옴
-        await ajax();
-        fetchAccommodation();
-    };
-
-    //페이지네이션
-    const handlePrevious = () => {
-        if (currentPage > 0) {
-            setCurrentPage(currentPage - 1); // 이전 페이지로 이동
-        }
-    };
-    const handleNext = async () => {
-        try {
-            const response = await axios.get(
-                `/v1/accommodation/member?page=${currentPage}`,
-                {
-                    params: {
-                        page: currentPage + 1, // 다음 페이지 번호 설정
-                    },
-                }
-            );
-            console.log(response);
-            setCurrentPage(currentPage + 1); // 페이지 번호 업데이트
-        } catch (e) {
-            console.log(e);
-        }
-    };
-
-    return (
-        <>
-            <div className="max-w-mw mx-auto pt-12 pb-12">
-                <div className="medium:flex medium:flex-row medium:gap-12  h-full  flex flex-col p-6 gap-12">
-                    <div className="medium:flex medium:flex-col medium:w-2/4 medium:p-4 w-full medium:h-full ">
-                        <TitleBtn
-                            TitleText="숙소 등록"
-                            LinkTo="/register"
-                            text="+ 숙소등록"
-                            className="mb-6"
-                        />
-                        <div className="flex justify-between flex-col medium:h-[515px] p-8 border-2 border-solid border-gray-300 rounded-xl">
-                            {isLoading ? (
-                                <ScaleLoader
-                                    color="#2393D2"
-                                    className="flex items-center justify-center "
-                                />
-                            ) : (
-                                <>
-                                    <div>
-                                        {accommodations.map((item, index) => (
-                                            <AccommodationItem
-                                                key={index}
-                                                id={item.id}
-                                                title={item.accommodationName}
-                                                text={item.id}
-                                                room={item.room.length}
-                                                type={item.accommodationType}
-                                                btn="true"
-                                                istrue={
-                                                    selectedRoomId === item.id
-                                                }
-                                                onClick={() =>
-                                                    handleRoomClick(item.id)
-                                                }
-                                                setAccommodations={
-                                                    setAccommodations
-                                                }
-                                                cookies={cookies}
-                                            />
-                                        ))}
-                                    </div>
-
-                                    <Pagination
-                                        onClick={handleNext}
-                                        handleNext={handleNext}
-                                        currentPage={currentPage}
-                                        setCurrentPage={setCurrentPage}
-                                        handlePrevious={handlePrevious}
-                                    />
-                                </>
-                            )}
-                        </div>
-                    </div>
-
-                    <div className="medium:flex medium:flex-col medium:w-2/4 medium:p-4 w-full">
-                        <TItle tag="h2" text="룸 조회" className="mb-8" />
-                        <div className="flex h-[515px] justify-between flex-col p-8 border-2 border-solid border-gray-300 rounded-xl overflow-y-auto">
-                            {selectedRoom.length > 0 ? (
-                                selectedRoom.map((roomItem, index) => (
-                                    <div key={index}>
-                                        {roomItem.room.map((item, index) => (
-                                            <RoomItem
-                                                key={index} // 선택된 방의 ID를 key로 사용
-                                                title={item.roomName}
-                                                text={item.roomInfo}
-                                                fixedMember={item.fixedMember}
-                                                maxedMember={item.maxedMember}
-                                                id={item.id}
-                                                roomImage={item.roomImage}
-                                            />
-                                        ))}
-                                    </div>
-                                ))
-                            ) : (
-                                <div className="flex flex-col gap-4 items-center h-full justify-center ">
-                                    <FaSearch className="text-7xl text-gray-300 mb-4" />
-                                    <p className="font-bold text-gray-500">
-                                        숙소를 클릭해 룸을 조회해주세요
-                                    </p>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="max-w-mw mx-auto pt-12 pb-12">
+        <div className="gap-8 medium:flex medium:flex-row medium:gap-8 h-full flex flex-col p-6">
+          <div className="medium:flex medium:flex-col medium:w-2/4 w-full medium:h-full ">
+            <TitleBtn
+              TitleText="숙소 등록"
+              LinkTo="/register"
+              text="+ 숙소등록"
+              className="mb-6"
+            />
+            <div className="flex justify-between flex-col medium:h-[560px] p-4 border-2 border-solid border-gray-300 rounded-xl">
+              {isLoading ? (
+                <ScaleLoader
+                  color="#2393D2"
+                  className="flex items-center justify-center "
+                />
+              ) : (
+                <>
+                  <div>
+                    {accommodations.map((item, index) => (
+                      <AccommodationItem
+                        key={index}
+                        id={item.id}
+                        title={item.accommodationName}
+                        text={item.id}
+                        room={item.room.length}
+                        type={item.accommodationType}
+                        btn="true"
+                        istrue={selectedRoomId === item.id}
+                        onClick={() => handleRoomClick(item.id)}
+                        setAccommodations={setAccommodations}
+                        cookies={cookies}
+                      />
+                    ))}
+                  </div>
+                  <Pagination
+                    onClick={handleNext}
+                    handleNext={handleNext}
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                    handlePrevious={handlePrevious}
+                  />
+                </>
+              )}
             </div>
-        </>
-    );
+          </div>
+
+          <div className="medium:flex medium:flex-col medium:w-2/4 w-full">
+            <TItle tag="h2" text="룸 조회" className="mb-8" />
+            <div className="flex h-[560px] justify-between flex-col p-4 border-2 border-solid border-gray-300 rounded-xl overflow-y-auto">
+              {selectedRoom.length > 0 ? (
+                selectedRoom.map((roomItem, index) => (
+                  <div key={index}>
+                    {roomItem.room.map((item, index) => (
+                      <RoomItem
+                        key={index} // 선택된 방의 ID를 key로 사용
+                        title={item.roomName}
+                        text={item.roomInfo}
+                        fixedMember={item.fixedMember}
+                        maxedMember={item.maxedMember}
+                        id={item.id}
+                        roomImage={item.roomImage}
+                      />
+                    ))}
+                  </div>
+                ))
+              ) : (
+                <div className="flex flex-col gap-4 items-center h-full justify-center ">
+                  <FaSearch className="text-7xl text-gray-300 mb-4" />
+                  <p className="font-bold text-gray-500">
+                    숙소를 클릭해 룸을 조회해주세요
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AdminPage;
